e11eddb4c41c464a2039743a4e2d2616e8afca57
leandrakell@gmail.com <leandrakell@gmail.com>
2023-05-17 02:21:06 -0300

    feat: implement Prisma database access

---
+++ api/src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get<string>('DATABASE_URL'),
+        },
+      },
+    });
+  }
+
+  cleanDb() {
+    return this.$transaction([
+      this.transaction.deleteMany(),
+      this.product.deleteMany(),
+      this.seller.deleteMany(),
+      this.user.deleteMany(),
+    ]);
+  }
+}
