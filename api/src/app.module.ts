070b59ac7e9eb65b7f8db2d50be57daad7913d08
leandrakell@gmail.com <leandrakell@gmail.com>
2023-05-17 02:16:13 -0300

    feat: set up api initial configs

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UserModule,
-    BookmarkModule,
+    TransactionModule,
     PrismaModule,
   ],
 })
